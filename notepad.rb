a00d32d99576998ea0d2b02ef7223bd32e3515b7
danikdesign <danik.newmail@gmail.com>
2022-05-04 23:29:41 +0300

    v1.01

--- notepad.rb
+++ notepad.rb
@@ -10,7 +10,7 @@ choices = Post.post_types
 
 choice = -1
 
-until choice > 0 && choice < choices.size
+until choice >= 0 && choice < choices.size
 
   choices.each_with_index do |type, index|
     puts "\t#{index}. #{type}"
