24c716a8eed61c3cbb80024badaffa550e81335c
danikdesign <danik.newmail@gmail.com>
2022-05-04 23:20:57 +0300

    version 1.0

--- task.rb
+++ task.rb
@@ -1,14 +1,26 @@
-
+require 'date'
 class Task < Post
   def initialize
     super
 
-    @die_date = Time.now
+    @due_date = Time.now
   end
 
   def read_from_console
+    puts "Что нужно сделать?"
+    @text = STDIN.gets.chomp
+
+    puts "К какому числу? Укажите дату в формате ДД.ММ.ГГГГ, напр. 01.01.1990"
+    input = STDIN.gets.chomp
+
+    @due_date = Date.parse(input)
   end
 
   def to_strings
+    time_string = "Создано: #{@created_at.strftime("%Y-%m-%d, %H:%M:%S")} \n\r \n\r"
+
+    deadline = "Крайний срок: #{@due_date}"
+
+    return [deadline, @text, time_string]
   end
 end
